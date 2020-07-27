5ca11c73f3eef4189f3cc0722766d3e0df20d1ab
keu <kevinsetup900@gmail.com>
2020-07-27 15:48:34 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LibroComponent } from './libro/libro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LibroComponent
   ],
   imports: [
     BrowserModule,
